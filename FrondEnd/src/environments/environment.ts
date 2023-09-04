478522900505efeeb3a4e2b63b4cdd7da415bd37
Samuel Paredes <samuelocta215@gmail.com>
2023-09-03 21:49:39 -0300

    Update

---
+++ FrondEnd/src/environments/environment.ts
@@ -0,0 +1,19 @@
+// This file can be replaced during build by using the `fileReplacements` array.
+// `ng build` replaces `environment.ts` with `environment.prod.ts`.
+// The list of file replacements can be found in `angular.json`.
+
+export const environment = {
+  firebase: {
+        apiKey: "",
+      authDomain: "",
+      projectId: "",
+      storageBucket: "",
+      messagingSenderId: "1",
+      appId: "",
+    },
+  production: false,
+
+  URL: 'your URL'
+};
+
+
